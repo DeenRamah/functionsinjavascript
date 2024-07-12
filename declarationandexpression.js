9bb873c00617a044fe3a46e2bf2a814f3a187b3d
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-07-13 00:16:12 +0300

    Update declarationandexpression.js

--- declarationandexpression.js
+++ declarationandexpression.js
@@ -1,8 +1,8 @@
-//Declaration
+//Declaration they are hoisted meaning they can be called before they are defined, functions are expressed as hoisted
 function add(a,b){
   return a + b;
 };
-
+//
 const minus = function(c,d){
   return c - d;
 };
