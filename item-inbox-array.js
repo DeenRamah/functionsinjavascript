8ad03fe9cd2ec31a149a7e292753b6d40563e1ad
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-07-12 23:48:42 +0300

    Create item-inbox-array.js

---
+++ item-inbox-array.js
@@ -0,0 +1,7 @@
+//item,inbox,array
+let num = [34,21,5,93];
+let filternum = num.filter((item,inbox,array) =>{
+  console.log(item)//34
+  console.log(index)//0
+  console.log(array)//[34,21,5,93]
+});
