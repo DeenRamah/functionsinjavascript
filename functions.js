24c98d5c17d54c2ee62e26d4a280fb8a0bf3ff22
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-07-13 00:11:44 +0300

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,6 @@
+//reusable blocks of code that performs a specific task, you can define once and call it multiple times with different inputs
+
+function greet(name){
+  return "Hello", + name + "!";
+}
+console.log(greet("Mike")); //output: Hell, Mike!
